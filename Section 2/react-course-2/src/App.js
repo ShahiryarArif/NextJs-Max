76bc64e3c7582a3faca939c41c95311229add65f
Shahiryar Arif <shahiryararif@Shahiryars-MacBook-Air.local>
2023-10-25 01:45:42 +0500

    Section 2:36 Adding Links & Navigation

--- Section 2/react-course-2/src/App.js
+++ Section 2/react-course-2/src/App.js
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetups from "./pages/AllMeetups";
 import NewMeetup from "./pages/NewMeetup";
 import Favourites from "./pages/Favourites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
-        <Route path="/" element={<AllMeetups/>}/>
-        <Route path="/new-meetup" element={<NewMeetup />}/>
+        <Route path="/" element={<AllMeetups />} />
+        <Route path="/new-meetup" element={<NewMeetup />} />
         <Route path="/favorites" element={<Favourites />} />
       </Routes>
     </div>
