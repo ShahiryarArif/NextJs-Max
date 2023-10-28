46a10dbb5e44dfb5f23af6005e1f5c36ea489bab
Shahiryar Arif <shahiryararif@Shahiryars-MacBook-Air.local>
2023-10-29 02:27:47 +0500

    Section 2:44 Sending a POST Http Request to Firebase

--- Section 2/react-course-2/src/pages/NewMeetup.js
+++ Section 2/react-course-2/src/pages/NewMeetup.js
@@ -2,11 +2,22 @@ import React from "react";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetup = () => {
-  function addMeetupHandler(meetupData) {}
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-course-nextjs-max-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 };
