6b38286eeca9ca06bf8c503792f1e7dad898f2a0
Shahiryar Arif <shahiryararif@Shahiryars-MacBook-Air.local>
2023-11-04 21:25:59 +0500

    Section 2:47 Using the useEffect Hook

--- Section 2/react-course-2/src/pages/AllMeetups.js
+++ Section 2/react-course-2/src/pages/AllMeetups.js
@@ -1,14 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const AllMeetups = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
-  fetch(
-    "https://react-course-nextjs-max-default-rtdb.firebaseio.com/meetups.json"
-  )
-    .then((response) => response.json())
-    .then(() => setIsLoading(false));
+
+  useEffect(() => {
+    fetch(
+      "https://react-course-nextjs-max-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
 
   if (isLoading) {
     return (
