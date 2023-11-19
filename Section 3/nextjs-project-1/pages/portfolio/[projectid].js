d25cd696c59d472e5bc42d2a4fa3495abe3ddb98
Shahiryar Arif <shahiryararif@Shahiryars-MacBook-Air.local>
2023-11-20 01:33:08 +0500

    Section 3:60 Adding Dynamic Paths & Routes

---
+++ Section 3/nextjs-project-1/pages/portfolio/[projectid].js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PortfolioProjectPage = () => {
+  return (
+    <div>
+      <h1>The PortfolioProjectPage</h1>
+    </div>
+  );
+};
+
+export default PortfolioProjectPage;
