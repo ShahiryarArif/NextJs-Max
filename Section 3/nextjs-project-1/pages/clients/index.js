25586cc8d7f32557b1bbf5f254755e6e33845f73
Shahiryar Arif <shahiryararif@Shahiryars-MacBook-Air.local>
2023-11-25 21:06:06 +0500

    Section 3:65 Navigating To Dynamic Routes

--- Section 3/nextjs-project-1/pages/clients/index.js
+++ Section 3/nextjs-project-1/pages/clients/index.js
@@ -1,9 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 const ClientsPage = () => {
+  const clients = [
+    { id: "sherry", name: "Shahiryar Arif" },
+    { id: "manu", name: "Manual" },
+  ];
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
