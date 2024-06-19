8a046bd84be38fa7955cb2331b888207d47ffe4f
Shahiryar Arif <shahiryararif@Shahiryars-MacBook-Air.local>
2024-06-20 03:41:55 +0500

    Dynamic Route in Parallel Route

--- Section 4 Routing & Page Rendering - Deep Dive/Routing&Page Rendering - Deep Dive/app/news/page.js
+++ Section 4 Routing & Page Rendering - Deep Dive/Routing&Page Rendering - Deep Dive/app/news/page.js
@@ -1,21 +1,11 @@
+import NewsList from "@/components/news-list";
 import { DUMMY_NEWS } from "@/dummy-news";
-import Image from "next/image";
-import Link from "next/link";
 
 export default function NewsPage() {
   return (
     <>
       <h1>News Page</h1>
-      <ul className="news-list">
-        {DUMMY_NEWS.map((newItem) => (
-          <li key={newItem.id}>
-            <Link href={`/news/${newItem.slug}`}>
-              <img src={`/images/news/${newItem.image}`} alt={newItem.title} />
-              <span>{newItem.title}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <NewsList news={DUMMY_NEWS} />
     </>
   );
 }
